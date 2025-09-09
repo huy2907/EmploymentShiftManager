c22ad27192d53753f80fd262c9c214838e4ce04f
huy2907 <wuywuy2907@gmail.com>
2025-09-09 10:34:46 +0700

    Add files via upload

---
+++ wwwroot/js/fullcalendar-init.js
@@ -0,0 +1,10 @@
+﻿
+// Giả lập mã khởi tạo FullCalendar
+document.addEventListener('DOMContentLoaded', function () {
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+        initialView: 'dayGridMonth',
+        events: '/calendar/events' // API giả lập
+    });
+    calendar.render();
+});
